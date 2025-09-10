7fbb9a6b21d5ac172f2f327d247288874acbc10f
Trimi123123 <trimmerovci10@gmail.com>
2025-09-10 19:14:56 +0200

    1
    1

---
+++ signup/login/signup.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+</head>
+<body>
+
+
+<form action="config.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+    
+
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</body>
+</html>
+
+<?php
+include_once("../database/config.php")
+
+
+
+$conn->execute($sql = "INSERT INTO Users (emri, mbiemri, email, password)
+VALUES ('?', '?', '?')");
+
+
+
+$stmt->bind_param('emri');
+$stmt->bind_param('mbiemri');
+$stmt->bind_param('email');
+$stmt->bind_param('password');
+
+
+
+?>
